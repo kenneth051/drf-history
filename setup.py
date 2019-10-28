c7d31ee7a293d4ab02e1acb8501fe300367d09c0
kenneth051 <kjdumba@gmail.com>
2019-10-28 16:33:35 +0300

    change readme file info

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="drf-history",
-    version="0.0.1",
+    version="0.0.2",
     packages=[
         "track_actions",
         "track_actions.migrations",
